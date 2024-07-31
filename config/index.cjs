5efe0256ae50f5cde0635ae2406f6b433a2a7df4
Fyfgughughh <146672142+Fyfgughughh@users.noreply.github.com>
2024-08-01 04:20:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '甘露',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oA-Di6tqNJQBBSTgeZ239PsIp_2A	',
+      id: 'oA-Di6tqNJQBBSTgeZ239PsIp_2A',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'vCa5hdRHsfP95whlbtUBpxIzQtSlIIV413a7JwK_Njc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'vCa5hdRHsfP95whlbtUBpxIzQtSlIIV413a7JwK_Njc',
 
   CALLBACK_USERS: [
     {
